6bf9e60714afad79e27ffd120ec129650cc1dc16
andrei.rad <andrei.rad@hpm.ro>
2022-08-29 08:14:17 +0300

    Add Kafka Consumers and Producers

---
+++ src/main/java/org/example/KafkaMessageHandler.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+/**
+ * The interface KafkaMessageHandler.
+ *
+ * This interface is the template callback functions that can
+ * be passed to an instance of the {@link org.example.SimpleConsumer}
+ */
+@FunctionalInterface
+public interface KafkaMessageHandler {
+    /**
+     * The method that defines the message processing behavior
+     *
+     * @param topicName The name of the topic being consumed
+     * @param message   The message that was consumed
+     * @throws Exception Thrown if an exception occurs
+     */
+    void processMessage(String topicName, ConsumerRecord<String, String> message) throws Exception;
+}
